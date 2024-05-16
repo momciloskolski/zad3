8723cfd136f97cd159100985356d4c2b55d783e2
momciloskolski <oopburner586@gmail.com>
2024-05-16 09:11:53 +0200

    pocetna verzija

---
+++ app/src/main/java/com/example/zad3/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.zad3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvGimnazija;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvGimnazija = (TextView) findViewById(R.id.textViewGimnazija);
+    }
+
+    public void onClickGimnazija(View view) {
+        tvGimnazija.setText("radi");
+    }
+
+
+    public void onClickbutton(View view){
+        textView.setText("Odeljenje III-5");
+    }
+}
